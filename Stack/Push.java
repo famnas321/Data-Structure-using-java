c4b15999ae4f491a2f9c396662a7d54216b8992d
famnas321 <famnaspk380@gmail.com>
2025-02-24 09:37:52 +0530

    committed on 24.2.2025

---
+++ Stack/Push.java
@@ -0,0 +1,54 @@
+public class Push {
+    
+ 
+        class Node{
+            int data;
+            Node next;
+            Node(int data){
+                this.data= data;
+            }
+        }
+      public Node top=null;
+      
+    
+      public void pushing(int data ){
+        Node newNode =new Node(data);
+        if (top==null){
+            top=newNode;
+            
+        }else{
+        newNode.next=top;
+        top=newNode;
+        }
+        
+    
+      }
+      public void display(){
+        Node temp= top;
+        if(temp == null){
+          System.out.print("Stack is empty");
+          return;
+        }
+       
+        while(temp !=null){
+          System.out.println(temp.data);
+          temp=temp.next;
+        }
+      }
+      
+    
+        public static void main(String[] args){
+           Push list =new Push();
+         
+          list.pushing(10);
+          list.pushing(40);
+          list.pushing(50);
+    
+          
+          
+          list.display();
+    
+        }
+    }
+    
+
